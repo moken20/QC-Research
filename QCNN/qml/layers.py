d4279adf3cd212829b11c1e5eb2935f8ea4e5d1e
Ryota Kemmoku <ryota.kemmoku@keio.jp>
2023-05-20 15:29:13 +0900

    layers.pyが完成

--- QCNN/qml/layers.py
+++ QCNN/qml/layers.py
@@ -81,8 +81,8 @@ def generate_uniformly_controlled_rotation(circ, params, control_qubit_indicies,
                 circ.cx(control_qubit_indicies[control_element], target_qubit_index)
                 break
 
-    print("--generate_uniformly_controlled_rotation------------------------")
-    circ.draw(output='mpl')
+    print("generate_uniformly_controlled_rotationの回路図")
+    print(circ)
 
     return
 
@@ -135,7 +135,7 @@ def legacy_conv4_layer_func(circ, params, active_qubits, barrier=True, kwargs={}
         circ.barrier()
 
     print("4qubit畳み込み層追加後")
-    circ.draw(output='mpl')
+    print(circ)
 
     return circ
 
@@ -173,7 +173,218 @@ def legacy_conv_layer_func(circ, params, active_qubits, barrier=True, kwargs={})
     if barrier:
         circ.barrier()
 
+    print("general畳み込み層追加後")
+    print(circ)
+
     return circ
 
 
+def legacy_pool_layer_func(circ, params, active_qubits, barrier=True, kwargs={}):
+
+    '''
+    プーリング層の作成
+
+    2x2のゲルマン行列とパラメータの線形結合をeの肩に乗せた行列をtarget qubitに作用させるような
+    controlled operatorを3qubitのグループに(0, 1)(2, 1)に設置して、真ん中のqubitに情報を凝縮
+    させる。
+    '''
+
+    pool_operators = generate_gell_mann(2)
+    v1 = get_conv_op(pool_operators. params[:3]) # first 3 parameters for V1, last 3 for V2
+    v2 = get_conv_op(pool_operators, params[3:])
+    v1_pool = qi.Operator(controlled_pool(v1))
+    v2_pool = qi.Operator(controlled_pool(v2))
+
+    if "start_index" in kwargs:
+        index = kwargs["start_index"]
+    else:
+        index = 0
+
+    if "label" in kwargs:
+        label = kwargs["label"]
+    else:
+        label = 'lp'
+
+    while index + 2 < len(active_qubits):
+        q_index = active_qubits[index]
+        q_index_1 = active_qubits[index + 1]
+        q_index_2 = active_qubits[index + 2]
+
+        circ.h(q_index)
+        circ.unitary(v1_pool, [q_index, q_index_1], label=label+'(1)')
+        circ.h(q_index_2)
+        circ.unitary(v2_pool, [q_index_2, q_index_1], label=label+'(2)')
+        index += 3
+
+    if barrier:
+        circ.barrier()
+
+    print("プーリング畳み込み層追加後")
+    print(circ)
+
+    return circ
+
+
+def legacy_fc_layer_fun(circ, params, active_qubits, barrier=True, kwargs={}):
+    '''
+    全結合層の作成
+    
+    active_qubits全部に作用するサイズのgell mann行列をparamと線形結合させ、
+    eの肩に乗っけて回路に設置
+    '''     
+
+    num_active_qubits = len(active_qubits)
+    fully_connected_mats = generate_gell_mann(2**num_active_qubits)
+    fully_connected_operator = get_conv_op(fully_connected_mats, params)
+
+    if "start_index" in kwargs:
+        index = kwargs["start_index"]
+    else:
+        index = 0
+
+    if "label" in kwargs:
+        label = kwargs["label"]
+    else:
+        label = 'fc'
+
+    circ.unitary(fully_connected_operator, active_qubits, label=label)
+
+    if barrier:
+        circ.barrier()
+
+    return circ
+
+def custom_conv_layer_fun(circ, params, active_qubits, barrier=True, kwargs={}):
+    '''
+    Jaybsoniさんoriginalの畳み込み層の作成
+
+    Lukinの元論文がgell mann行列を使っているのに対し、彼はuniformlly controlled rotationを利用
+    していて、パラメータ数を大幅に減らしている
+    '''
+
+    if "start_index" in kwargs:
+        index = kwargs["start_index"]
+    else:
+        index = 0
+
+    if "label" in kwargs:
+        label = kwargs['label']
+    else:
+        label = 'cc'
+
+    if "group_size" in kwargs:
+        group_size = kwargs["group_size"]
+    else:
+        group_size = 3
+
+    while index + (group_size - 1) < len(active_qubits):
+        param_pointer = 0
+        lst_indicies = range(index, index + group_size)
+
+        #z, y ascending loop
+        for axis in ['z', 'y']:
+            split_index = group_size - 1
+            while split_index > 0:
+                control_indicies = lst_indicies[:split_index]
+                control_qubit_indicies = [active_qubits[i] for i in control_indicies]
+                target_qubit_index = active_qubits[lst_indicies[split_index]]
+
+                num_local_params = 2**(len(control_qubit_indicies))
+                local_params = params[param_pointer:param_pointer + num_local_params]
+                param_pointer += num_local_params
+
+                generate_uniformly_controlled_rotation(circ, local_params, control_qubit_indicies,
+                                                       target_qubit_index, axis=axis, label=label)
+                
+                split_index -= 1
+
+            if axis == 'z':
+                circ.rz(params[param_pointer], active_qubits[lst_indicies[split_index]])
+            else:
+                circ.ry(params[param_pointer], active_qubits[lst_indicies[split_index]])
+            param_pointer += 1
+
+
+        #descending loop
+        for axis in ['y', 'z']:
+            split_index = 1
+
+            if axis == 'z':
+                circ.rz(params[param_pointer], active_qubits[lst_indicies[split_index-1]])
+                param_pointer += 1
+
+            while split_index < group_size:
+                control_indicies = lst_indicies[:split_index]
+                control_qubit_indicies = [active_qubits[i] for i in control_indicies]
+                target_qubit_index = active_qubits[lst_indicies[split_index]]
+
+                num_local_params = 2**(len(control_qubit_indicies))
+                local_params = params[param_pointer:param_pointer + num_local_params]
+                param_pointer += num_local_params
+
+                generate_uniformly_controlled_rotation(circ, local_params, control_qubit_indicies,
+                                                       target_qubit_index, axis=axis, label=label)
+                split_index += 1
+
+        index += group_size
+
+    if barrier:
+        circ.barrier()
+
+    return
+
+
+# Layer class ###############################################
+class Layer:
+    '''
+    引数で指定できる必要なレイヤーをまとめるクラス
+    '''
+
+    def __init__(self, name, func, param_shape):
+        self.name = name  #layerのlabel
+        self.func = func  #回路に呼び出す関数
+        self.shape_params = param_shape  # レイヤーに必要なパラメータ数
+        return
+    
+    def apply_layer(self, circ, params, active_qubits, kwargs={}):
+        inst = self.func(circ, params, active_qubits, kwargs=kwargs)
+        return inst
+    
+
+# Functins to get customizable layers ############################
+def get_legacy_fc_layer(num_active_qubits):
+    '''
+    fully connecter layerのインスタンスを作成する
+    '''
+    layer_name = "legacy_fc_layer_n{}".format(num_active_qubits)
+    fc_layer = Layer(layer_name, legacy_fc_layer_fun, (2**num_active_qubits - 1,))
+    return fc_layer
+
+
+def get_cunstom_conv_layer(group_size):
+    '''
+    custom conv layerのインスタンスを作成する
+    '''
+    num_params = 0
+    for q in range(group_size):
+        num_params += 2 ** 9
+    num_params = (num_params * 2 - 1) * 2 + 1   #詳細はhttps://arxiv.org/pdf/quant-ph/0407010.pdf
+
+    layer_name = "custom_conv_layer_n{}".format(group_size)
+    cc_layer = Layer(layer_name, custom_conv_layer_fun, (num_params,))
+    return cc_layer
+
+
+# Base Legacy Layers #############################################
+legacy_conv4_layer = Layer("legacy_conv4_layer", legacy_conv4_layer_func, (15,))
+legacy_conv_layer = Layer("legacy_conv_layer", legacy_conv_layer_func, (63,))
+legacy_pool_layer = Layer("legacy_pool_layer", legacy_pool_layer_func, (6,))
+
+#この3つのlayerはパラメータ数が固定なのであらかじめインスタンスを作成しておける
+
+def main():
+    return
+
+if __name__=="__main__":
+    main()
 
